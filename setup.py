e2c0b90a3e9ac2f38344c1d4ba87f4fd6b6bff11
meyt <pasd3000@gmail.com>
2017-04-25 23:34:11 +0430

    Update setup

--- setup.py
+++ setup.py
@@ -1,31 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 readme = open('README.rst').read()
 
-setup(name='microhttp',
-      version='0.1.1',
-      description='A tool-chain for nanohttp',
-      long_description=readme,
-      url='http://github.com/meyt/microhttp',
-      author='Mahdi Ghane.g',
-      license='MIT',
-      keywords='nanohttp microhttp web tool-chain',
-      classifiers=[
-          'Development Status :: 1 - Planning',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: POSIX :: Linux',
-          'Operating System :: Unix',
-          'Programming Language :: Python :: 3 :: Only',
-          'Topic :: Software Development :: Libraries'
-      ],
-      install_requires=[
-          'nanohttp',
-          'dogpile.cache',
-          'sqlalchemy',
-          'mako'
-      ],
-      packages=['microhttp'],
-      )
-
-
-
+setup(
+    name='microhttp',
+    version='0.1.2',
+    description='A tool-chain for creating web application based on nanohttp',
+    long_description=readme,
+    url='http://github.com/meyt/microhttp',
+    author='Mahdi Ghane.g',
+    license='MIT',
+    keywords='nanohttp microhttp web tool-chain',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'nanohttp',
+        'dogpile.cache',
+        'sqlalchemy',
+        'webtest',
+        'mako'
+    ]
+)
