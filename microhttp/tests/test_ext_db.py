4dd188320ab6e2c3dfeece9c7633e5e9d74b3b14
meyt <pasd3000@gmail.com>
2017-05-07 12:43:52 +0430

    add default database tests

--- microhttp/tests/test_ext_db.py
+++ microhttp/tests/test_ext_db.py
@@ -13,7 +13,16 @@ class TestCase(WebTestCase):
         class Root(Controller):
             @html
             def index(self):
-                db_session = db.get_session('db1')
+                db_session = db.get_session()
+                try:
+                    db_session.execute('SELECT * FROM sqlite_master')
+                    return ''
+                except:
+                    raise InternalServerError
+
+            @html
+            def db2(self):
+                db_session = db.get_session('db2')
                 try:
                     db_session.execute('SELECT * FROM sqlite_master')
                     return ''
@@ -23,7 +32,15 @@ class TestCase(WebTestCase):
         def __init__(self):
             self.builtin_configuration += """
 sqlalchemy:
-  db1:
+  default:
+    engine:
+      name_or_url: 'sqlite:///:memory:'
+      echo: false
+    session:
+      autoflush: True
+      autocommit: False
+      expire_on_commit: True
+  db2:
     engine:
       name_or_url: 'sqlite:///:memory:'
       echo: false
@@ -39,6 +56,10 @@ sqlalchemy:
 
     def test_simple(self):
         resp = self.app.get('/')
+        assert resp.status_int == 200
+
+    def test_another_database(self):
+        resp = self.app.get('/db2')
         assert resp.status_int == 200
 
 if __name__ == '__main__':
