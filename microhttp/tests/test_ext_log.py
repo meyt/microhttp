f20905da6ea6624e652c2c6ffb40e0fa143cbe67
meyt <pasd3000@gmail.com>
2018-03-02 01:06:52 +0330

    Add log tests

---
+++ microhttp/tests/test_ext_log.py
@@ -0,0 +1,35 @@
+import unittest
+from nanohttp import Controller, html
+from microhttp import Application as BaseApplication
+from microhttp.ext import log
+from microhttp.tests.helpers import WebTestCase
+
+
+class TestCase(WebTestCase):
+
+    # noinspection PyAbstractClass
+    class Application(BaseApplication):
+        class Root(Controller):
+            @html
+            def index(self):
+                log.exception('there is an exception')
+                log.error('there is an error')
+                log.info('there is an info message')
+                log.critical('there is a critical message')
+                log.debug('there is a debug message')
+                log.warning('there is a warning message')
+                log.get_logger().warning('another warning message')
+                return ''
+
+        def __init__(self):
+            super().__init__(self.Root())
+
+        def configure(self, *args, **kwargs):
+            super().configure(*args, **kwargs)
+
+    def test_log(self):
+        self.app.get('/', status=200)
+
+
+if __name__ == '__main__':  # pragma: nocover
+    unittest.main()
