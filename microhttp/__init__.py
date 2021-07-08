9cb4a231466a81ae350c5a2370350db06c81dd06
meyt <pasd3000@gmail.com>
2021-07-08 20:37:46 +0430

    v0.14.6

--- microhttp/__init__.py
+++ microhttp/__init__.py
@@ -2,6 +2,6 @@ from .application import Application
 from .bus import bus
 from .exceptions import MicrohttpException
 
-__version__ = "0.14.5"
+__version__ = "0.14.6"
 
 __all__ = (Application, bus, MicrohttpException)
